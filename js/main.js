2f2eefca097a5855f33b07c641f54552ad52f3c4
fjguipen <fjguipen@gmail.com>
2018-10-21 23:08:23 +0200

    Ajustes. Falta formulario

--- js/main.js
+++ js/main.js
@@ -1,13 +1,40 @@
 window.onload = function (){
-    loading()
+    loading();
+    fadeIn();
 }
 
 
 function loading (){
     let body = document.querySelector('body');
-    let loader = document.querySelector('.loader')
+    let loader = document.querySelector('.loader');
  
     loader.classList.add('d-none');
-    body.classList.remove('no-scroll')
+    body.classList.remove('no-scroll');
 
 }
+
+function fadeIn(){
+    let elemento = document.querySelector('.fading')
+
+    elemento.classList.add('fadeOn')
+
+    
+}
+
+function openForm(){
+    let form = document.querySelector('#form');
+    let body = document.querySelector('body');
+    
+    body.classList.add('no-scroll');
+    form.classList.remove('d-none')
+}
+
+function closeForm(event){
+    let form = document.querySelector('#form');
+    let body = document.querySelector('body');
+
+    event.preventDefault()
+    body.classList.remove('no-scroll');
+    form.classList.add('d-none')
+}
+
